b4c3d87360289c78cd0b1076bafdd91d376aaa21
Mathias Rav <rav@cs.au.dk>
2013-06-02 19:25:17 +0200

    Add alias list parsing and refactor.

--- fbtk.js
+++ fbtk.js
@@ -2,31 +2,6 @@
 // Date: May 22, 2013
 // rød grød med fløde æ ø å
 
-function insert_alias(str) {
-	return function (n, orig_string) {
-		var replaced = document.createElement('span');
-		replaced.title = orig_string;
-		replaced.className = 'tk_title';
-		replaced.textContent = str;
-		n.parentNode.insertBefore(replaced, n);
-	};
-}
-
-function ReplacementList(arg, fn, target) {
-	var f = function (arg, fn, target) {
-		var replacer = fn(arg);
-		if ('string' === typeof(replacer)) {
-			replacer = insert_alias(replacer);
-		}
-		f.replacements[target] = replacer;
-		f.targets.push(target);
-		return f;
-	};
-	f.replacements = {};
-	f.targets = [];
-	return f(arg, fn, target);
-}
-
 function year_prefix(year) {
 	year = 2012 - year;
 	var prefixes = ['', 'G', 'B', 'O', 'TO'];
@@ -54,38 +29,8 @@ function title_bling(title) {
 	return title;
 }
 
-function t(title, year) {
-	return title_prefix(title) + year_prefix(year) + title_bling(title);
-}
-
-function mk_t(title) {
-	return function (year) {
-		return t(title, year);
-	}
-}
-
-var FORM = mk_t('FORM'),
-    NF = mk_t('NF'),
-    KASS = mk_t('KASS'),
-    CERM = mk_t('CERM'),
-    VC = mk_t('VC'),
-    SEKR = mk_t('SEKR'),
-    PR = mk_t('PR');
-
-function FU(title) {
-	return function (year) {
-		return ((title == 'AN')
-			? (year_prefix(year) + 'FU' + title)
-			: ('FU' + title));
-	}
-}
-
-function h(s) {
-	return s;
-}
-
-var TK_base = (
-'<span style="font-family: monospace; display: inline-block; white-space: nowrap"><span '+
+var TK = (
+'<span '+
 'style="vertical-align: -0.4pt">T</span><span '+
 'style="font-weight: bold">&Aring;</span>G<span '+
 'style="display: inline-block; transform: rotate(8deg)">E</span><span '+
@@ -93,126 +38,93 @@ var TK_base = (
 'style="vertical-align: 0.2pt; font-weight: bold">A</span><span '+
 'style="vertical-align: -0.6pt">M</span><span '+
 'style="display: inline-block; transform: rotate(-8deg); font-weight: bold">M</span>ER');
-var TK = TK_base + '</span>';
-var TKET = TK_base + '<span style="vertical-align: 0.6pt">ET</span></span>';
+var TKET = TK + '<span style="vertical-align: 0.6pt">ET</span>';
 
-function insert_html(h) {
+function insert_TK_html(h) {
 	return function (n, orig_string) {
 		var replaced = document.createElement('span');
+		replaced.style.fontFamily = 'monospace';
+		replaced.style.display = 'inline-block';
+		replaced.style.whiteSpace = 'nowrap';
 		replaced.innerHTML = h;
 		n.parentNode.insertBefore(replaced, n);
 	};
 }
 
-var aliases = (ReplacementList
-(2003, CERM, 'Sune S. Thomsen')
-(2004, VC,   'Christina Koch Perry')
-(2005, CERM, 'Ole Søe Sørensen')
-(2005, FORM, 'Dan Beltoft')
-(2005, KASS, 'Tanja Kragbæk')
-(2005, VC,   'Rasmus Ragnvald Olofsson')
-(2006, CERM, 'Tine Vraast-Thomsen')
-(2006, FORM, 'Michael Volf Henneberg')
-(2006, KASS, 'Lars Thorhauge')
-(2006, NF,   'Aslak Thorndahl Lindballe')
-(2006, PR,   'Mia Dyhr Christensen')
-(2006, SEKR, 'Lasse Vilhelmsen')
-(2006, VC,   'Martin Studsgaard Christensen')
-(2007, CERM, 'Johan Sigfred Abildskov')
-(2007, FORM, 'Jan Munksgård Baggesen')
-(2007, KASS, 'Mads Baggesen')
-(2007, NF,   'Eva Lykkegaard Poulsen')
-(2007, PR,   'Sidse Damgaard')
-(2007, SEKR, 'Ninni Schaldemose')
-(2007, VC,   'Kasper Søgaard Deleuran')
-(2008, CERM, 'Sofie Kastbjerg')
-(2008, FORM, 'Tue Christensen')
-(2008, PR,   'Cecilie Vahlstrup')
-(2009, CERM, 'Christian Bladt Brandt')
-(2009, FORM, 'Jonas Bæklund')
-(2009, KASS, 'Rikke Aagaard')
-(2009, NF,   'Ditte Både Sandkamm')
-(2009, PR,   'Nikolaj Andresen')
-(2009, SEKR, 'Anne Clemmensen')
-(2009, VC,   'Troels Tinggaard Hahn')
-(2010, FORM, 'Mie Elholm Birkbak')
-(2010, KASS, 'Torben Muldvang Andersen')
-(2010, NF,   'Morten Jensen')
-(2010, PR,   'Kristoffer L. Winge')
-(2010, SEKR, 'Maria Kragelund')
-(2010, VC,   'Morten Rasmussen')
-(2011, CERM, 'Sabrina Tang Christensen')
-(2011, FORM, 'Jakob Schultz-Nielsen')
-(2011, KASS, 'Britt Fredsgaard')
-(2011, NF,   'Kasper Monrad')
-(2011, PR,   'Marie Kirkegaard')
-(2011, SEKR, 'Niels Ramskov Bøje')
-(2011, VC,   'Maiken Haahr Hansen')
-(2012, CERM, 'Mads Fabricius')
-(2012, FORM, 'Steffen Videbæk Petersen')
-(2012, KASS, 'Eva Gjaldbæk Frandsen')
-(2012, NF,   'Johannes Christensen')
-(2012, PR,   'Nana Halle')
-(2012, SEKR, 'Jakob Rørsted Mosumgaard')
-(2012, VC,   'Peter Slemming-Adamsen')
-(2005, FU('JA'), 'Jane Drejer')
-(2006, FU('ZA'), 'Sarah Zakarias')
-(2007, FU('AN'), 'Kenneth Sejdenfaden Bøgh')
-(2007, FU('HO'), 'Daniel Dalhoff Hviid')
-(2007, FU('ME'), 'Mette Aagaard')
-(2008, FU('AN'), 'Andreas Sand Gregersen')
-(2008, FU('NÉ'), 'René Søndergaard')
-(2008, FU('RU'), 'Jesper Unna')
-(2009, FU('BS'), 'Sandra Pedersen')
-(2009, FU('HN'), 'Mette Hansen')
-(2009, FU('XA'), 'Christian Fretté')
-(2009, FU('ØL'), 'Carina Kjeldahl Møller')
-(2009, FU('ØR'), 'Signe Grønborg')
-(2010, FU('AN'), 'Andreas Nikolai Kyed Bovin')
-(2010, FU('NI'), 'Asbjørn Stensgaard')
-(2010, FU('PH'), 'Pernille Hornemann Jensen')
-(2010, FU('UL'), 'Camilla Pedersen')
-(2010, FU('RØ'), 'Lærke Rønlev Reinholdt')
-(2011, FU('FR'), 'Anders Friis Jensen')
-(2011, FU('HR'), 'Christina Moeslund')
-(2011, FU('IØ'), 'Mathilde Biørn Madsen')
-(2011, FU('LA'), 'Camilla Skree Sørensen')
-(2011, FU('NU'), 'Andreas Bendix Nuppenau')
-(2011, FU('RI'), 'Frederik Jerløv')
-(2011, FU('RT'), 'Martin Sand')
-(2012, FU('AN'), 'Henrik Lund Mortensen')
-(2012, FU('CO'), 'Jacob Schnedler')
-(2012, FU('HI'), 'Mathias Dannesbo')
-(2012, FU('LI'), 'Line Bjerg Sørensen')
-(2012, FU('LO'), 'Lone Koed')
-(2012, FU('LS'), 'Sara Poulsen')
-(2012, FU('MO'), 'Marianne Ostenfeldt Mortensen')
-(2012, FU('MY'), 'Rasmus Thygesen')
-(2012, FU('NA'), 'Karina Sunds Nielsen')
-(2012, FU('ZU'), 'Christian Bonar Zeuthen')
-('ADAM',      h, 'Adam Etches')
-('Nissen',    h, 'Anders Hauge Nissen')
-('Metal Bo',  h, 'Bo Mortensen')
-('CBM',       h, 'Christian Brandt Møller')
-('J-DAG',     h, 'Jacob Damgaard Jensen')
-('Jen_s',     h, 'Jens Kusk Block')
-('EFUIT',     h, 'Lauge Mølgaard Hoyer')
-('Have',      h, 'Martin Anker Have')
-('Mavraganis',h, 'Mathias Jaquet Mavraganis')
-('Rav',       h, 'Mathias Rav')
-('Metten',    h, 'Mette Lysgaard Schulz')
-('Vester',    h, 'Mikkel Bak Vester')
-('M3',        h, 'Morten Schaumburg')
-('Cramer',    h, 'Morten \'Cramer\' Nikolaj Pløger')
-('#',         h, 'Rikke Hein')
-('Sean',      h, 'Sean Geggie')
-(insert_html(TKET), h, 'TÅGEKAMMERET')
-(insert_html(TK),   h, 'TÅGEKAMMER')
-);
+var aliases = {'replacements': {}, 'targets': []};
+function add_alias(source, destination) {
+	aliases.targets.push(source);
+	aliases.replacements[source] = destination;
+}
+add_alias('TÅGEKAMMERET', insert_TK_html(TKET));
+add_alias('TÅGEKAMMER', insert_TK_html(TK));
 
-var alias_regexp = new RegExp(aliases.targets.join('|'));
+var alias_regexp;
+function compute_alias_regexp() {
+	alias_regexp = new RegExp(aliases.targets.join('|'));
+}
+compute_alias_regexp();
 
-//Sune Riise Nielsen: T^78OVC (1931)
+function parse_alias(line) {
+	var prefixed = /^(\d+) +([^ ]+) +(.*)/.exec(line);
+	if (prefixed) {
+		return {'name': prefixed[3],
+			'year': prefixed[1],
+			'title': prefixed[2]};
+	}
+	var hangaround = /^"([^"]*)" +(.*)/.exec(line);
+	if (hangaround) {
+		return {'name': hangaround[2],
+			'title': hangaround[1]};
+	}
+	return null;
+}
+
+function parse_aliases(input, cb) {
+	var lineMatch;
+	var re = /^.+$/mg;
+	while (lineMatch = re.exec(input)) {
+		var line = lineMatch[0];
+		var parsed = parse_alias(line);
+		cb(parsed, line);
+	}
+}
+
+function make_title(o) {
+	var title = o.title;
+	var fancy = title_bling(o.title);
+	if ('year' in o) {
+		var year = o.year;
+		if (title == 'FUAN') {
+			fancy = year_prefix(year) + fancy;
+		} else if (title.substring(0, 2) != 'FU') {
+			fancy = year_prefix(year) + fancy;
+		} else {
+			// FU; no prefix
+		}
+	}
+	return title_prefix(title) + fancy;
+}
+
+function insert_alias(str) {
+	return function (n, orig_string) {
+		var replaced = document.createElement('span');
+		replaced.title = orig_string;
+		replaced.className = 'tk_title';
+		replaced.textContent = str;
+		n.parentNode.insertBefore(replaced, n);
+	};
+}
+
+function add_parsed_alias(o, origLine) {
+	if (!o) {
+		// parse error
+		console.log("Failed to parse input line: ["+origLine+"]");
+		return;
+	}
+	var title = make_title(o);
+	add_alias(o.name, insert_alias(title));
+}
 
 // Recursively apply transformation to all text nodes in a DOM subtree.
 function r(n) {
@@ -261,5 +173,113 @@ window.theTKTitleObserver.observe(document.body, {
 	'subtree': true
 });
 
-// Apply transformation initially.
-r(document.body);
+function add_aliases(input) {
+	parse_aliases(input, add_parsed_alias);
+	compute_alias_regexp();
+	r(document.body);
+}
+
+add_aliases(
+'2003 CERM Sune S. Thomsen\n'+
+'2004 VC   Christina Koch Perry\n'+
+'2005 CERM Ole Søe Sørensen\n'+
+'2005 FORM Dan Beltoft\n'+
+'2005 KASS Tanja Kragbæk\n'+
+'2005 VC   Rasmus Ragnvald Olofsson\n'+
+'2006 CERM Tine Vraast-Thomsen\n'+
+'2006 FORM Michael Volf Henneberg\n'+
+'2006 KASS Lars Thorhauge\n'+
+'2006 NF   Aslak Thorndahl Lindballe\n'+
+'2006 PR   Mia Dyhr Christensen\n'+
+'2006 SEKR Lasse Vilhelmsen\n'+
+'2006 VC   Martin Studsgaard Christensen\n'+
+'2007 CERM Johan Sigfred Abildskov\n'+
+'2007 FORM Jan Munksgård Baggesen\n'+
+'2007 KASS Mads Baggesen\n'+
+'2007 NF   Eva Lykkegaard Poulsen\n'+
+'2007 PR   Sidse Damgaard\n'+
+'2007 SEKR Ninni Schaldemose\n'+
+'2007 VC   Kasper Søgaard Deleuran\n'+
+'2008 CERM Sofie Kastbjerg\n'+
+'2008 FORM Tue Christensen\n'+
+'2008 PR   Cecilie Vahlstrup\n'+
+'2009 CERM Christian Bladt Brandt\n'+
+'2009 FORM Jonas Bæklund\n'+
+'2009 KASS Rikke Aagaard\n'+
+'2009 NF   Ditte Både Sandkamm\n'+
+'2009 PR   Nikolaj Andresen\n'+
+'2009 SEKR Anne Clemmensen\n'+
+'2009 VC   Troels Tinggaard Hahn\n'+
+'2010 FORM Mie Elholm Birkbak\n'+
+'2010 KASS Torben Muldvang Andersen\n'+
+'2010 NF   Morten Jensen\n'+
+'2010 PR   Kristoffer L. Winge\n'+
+'2010 SEKR Maria Kragelund\n'+
+'2010 VC   Morten Rasmussen\n'+
+'2011 CERM Sabrina Tang Christensen\n'+
+'2011 FORM Jakob Schultz-Nielsen\n'+
+'2011 KASS Britt Fredsgaard\n'+
+'2011 NF   Kasper Monrad\n'+
+'2011 PR   Marie Kirkegaard\n'+
+'2011 SEKR Niels Ramskov Bøje\n'+
+'2011 VC   Maiken Haahr Hansen\n'+
+'2012 CERM Mads Fabricius\n'+
+'2012 FORM Steffen Videbæk Petersen\n'+
+'2012 KASS Eva Gjaldbæk Frandsen\n'+
+'2012 NF   Johannes Christensen\n'+
+'2012 PR   Nana Halle\n'+
+'2012 SEKR Jakob Rørsted Mosumgaard\n'+
+'2012 VC   Peter Slemming-Adamsen\n'+
+'2005 FUJA Jane Drejer\n'+
+'2006 FUZA Sarah Zakarias\n'+
+'2007 FUAN Kenneth Sejdenfaden Bøgh\n'+
+'2007 FUHO Daniel Dalhoff Hviid\n'+
+'2007 FUME Mette Aagaard\n'+
+'2008 FUAN Andreas Sand Gregersen\n'+
+'2008 FUNÉ René Søndergaard\n'+
+'2008 FURU Jesper Unna\n'+
+'2009 FUBS Sandra Pedersen\n'+
+'2009 FUHN Mette Hansen\n'+
+'2009 FUXA Christian Fretté\n'+
+'2009 FUØL Carina Kjeldahl Møller\n'+
+'2009 FUØR Signe Grønborg\n'+
+'2010 FUAN Andreas Nikolai Kyed Bovin\n'+
+'2010 FUNI Asbjørn Stensgaard\n'+
+'2010 FUPH Pernille Hornemann Jensen\n'+
+'2010 FUUL Camilla Pedersen\n'+
+'2010 FURØ Lærke Rønlev Reinholdt\n'+
+'2011 FUFR Anders Friis Jensen\n'+
+'2011 FUHR Christina Moeslund\n'+
+'2011 FUIØ Mathilde Biørn Madsen\n'+
+'2011 FULA Camilla Skree Sørensen\n'+
+'2011 FUNU Andreas Bendix Nuppenau\n'+
+'2011 FURI Frederik Jerløv\n'+
+'2011 FURT Martin Sand\n'+
+'2012 FUAN Henrik Lund Mortensen\n'+
+'2012 FUCO Jacob Schnedler\n'+
+'2012 FUHI Mathias Dannesbo\n'+
+'2012 FULI Line Bjerg Sørensen\n'+
+'2012 FULO Lone Koed\n'+
+'2012 FULS Sara Poulsen\n'+
+'2012 FUMO Marianne Ostenfeldt Mortensen\n'+
+'2012 FUMY Rasmus Thygesen\n'+
+'2012 FUNA Karina Sunds Nielsen\n'+
+'2012 FUZU Christian Bonar Zeuthen\n'+
+'"ADAM"       Adam Etches\n'+
+'"Nissen"     Anders Hauge Nissen\n'+
+'"Metal Bo"   Bo Mortensen\n'+
+'"CBM"        Christian Brandt Møller\n'+
+'"J-DAG"      Jacob Damgaard Jensen\n'+
+'"Jen_s"      Jens Kusk Block\n'+
+'"EFUIT"      Lauge Mølgaard Hoyer\n'+
+'"Have"       Martin Anker Have\n'+
+'"Mavraganis" Mathias Jaquet Mavraganis\n'+
+'"Rav"        Mathias Rav\n'+
+'"Metten"     Mette Lysgaard Schulz\n'+
+'"Vester"     Mikkel Bak Vester\n'+
+'"M3"         Morten Schaumburg\n'+
+'"Cramer"     Morten \'Cramer\' Nikolaj Pløger\n'+
+'"#"          Rikke Hein\n'+
+'"Sean"       Sean Geggie\n'+
+''
+);
