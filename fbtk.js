ff2c8a3f86d570455eedbfab6ebe315e4d6d478e
Mathias Rav <rav@cs.au.dk>
2013-10-13 17:15:27 +0200

    Add keypress handler; add setting to always use prefix in FU names; use explicit stack in DOM traversal

--- fbtk.js
+++ fbtk.js
@@ -5,17 +5,29 @@
 
 var C = ('<span style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif;">'
 +'\u2102</span>');
-window.TKconfig = ({
+window['TKconfig'] = ({
+	// We don't write RemToR anywhere
 	//'epsilon': 'e',
+	// Dollar sign used to write KA$$
 	'dollar': '$',
+	// C used to write CERM
 	'cermC': C,
+	// C used to write VC
 	'vcC': C,
+	// Whether exponents should be written as superscripts
 	'eksponent': true,
-	'gf': 2013
+	// From which year prefixes are computed
+	'gf': 2013,
+	// True if we should write a prefix in front of a FU title
+	'FUprefix': false
 });
 
+function TK(key) {
+	return window['TKconfig'][key];
+}
+
 function year_prefix(year) {
-	year = window.TKconfig['gf'] - year;
+	year = TK('gf') - year;
 	var prefixes = ['', 'G', 'B', 'O', 'TO'];
 	if (0 <= year && year < prefixes.length) {
 		return prefixes[year];
@@ -26,7 +38,7 @@ function year_prefix(year) {
 	if (year < 0) { exponent = -year; negative = true; }
 	exponent = exponent+'';
 	var exponentString = '';
-	if (window.TKconfig['eksponent']) {
+	if (TK('eksponent')) {
 		var s = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
 		for (var i = 0; i < exponent.length; ++i)
 			exponentString += s[exponent.charCodeAt(i) - 48];
@@ -37,16 +49,16 @@ function year_prefix(year) {
 }
 
 function title_bling(title) {
-	if (title == "KASS") return "KA" + window.TKconfig['dollar'] + window.TKconfig['dollar'];
-	if (title == 'CERM') return window.TKconfig['cermC'] + "ERM";
-	if (title == 'VC') return "V" + window.TKconfig['vcC'];
+	if (title == "KASS") return "KA" + TK('dollar') + TK('dollar');
+	if (title == 'CERM') return TK('cermC') + "ERM";
+	if (title == 'VC') return "V" + TK('vcC');
 
 	return title;
 }
 
 var svg = {};
 
-var TK = (
+var html_TK = (
 '<span '+
 'style="vertical-align: -0.4pt">T</span><span '+
 'style="font-weight: bold">&Aring;</span>G<span '+
@@ -57,7 +69,7 @@ var TK = (
 'style="vertical-align: -0.6pt">M</span><span '+
 'style="display: inline-block; transform: rotate(-8deg); -webkit-transform: '+
 'rotate(-8deg); font-weight: bold">M</span>ER');
-var TKET = TK + '<span style="vertical-align: 0.6pt">ET</span>';
+var html_TKET = html_TK + '<span style="vertical-align: 0.6pt">ET</span>';
 
 ///////////////////////////////////////////////////////////////////////////////
 // Callback generator for add_alias.
@@ -103,8 +115,8 @@ function add_alias(source, destination) {
 	aliases.replacements[source] = destination;
 }
 
-add_alias('TÅGEKAMMERET', insert_TK_html(TKET));
-add_alias('TÅGEKAMMER', insert_TK_html(TK));
+add_alias('TÅGEKAMMERET', insert_TK_html(html_TKET));
+add_alias('TÅGEKAMMER', insert_TK_html(html_TK));
 
 var alias_regexp;
 function compute_alias_regexp() {
@@ -152,15 +164,21 @@ function make_title(o) {
 	var fancy = title_bling(o['title']);
 	if ('year' in o) {
 		var year = o['year'];
+		var addPrefix = false;
 		if (title == 'FUAN') {
-			fancy = year_prefix(year) + fancy;
+			addPrefix = true;
 		} else if (title == 'FU') {
 			// Unnamed FU (e.g. KFU); always show prefix
-			fancy = year_prefix(year) + fancy;
+			addPrefix = true;
 		} else if (title.substring(0, 2) != 'FU') {
-			fancy = year_prefix(year) + fancy;
+			// BEST title
+			addPrefix = true;
 		} else {
-			// FU; no prefix
+			// Ordinary FU
+			addPrefix = TK('FUprefix');
+		}
+		if (addPrefix) {
+			fancy = year_prefix(year) + fancy;
 		}
 	}
 	return fancy;
@@ -204,55 +222,60 @@ function add_parsed_alias(o, origLine) {
 ///////////////////////////////////////////////////////////////////////////////
 // Recursively apply transformation to all text nodes in a DOM subtree.
 ///////////////////////////////////////////////////////////////////////////////
-function r(n) {
-	if (n.nodeType == 1) {
-		// Recurse through all child nodes of this DOM element.
-		var c = n.firstChild;
-		while (c) {
-			var n = c.nextSibling;
-			r(c);
-			c = n;
-		}
-	} else if (n.nodeType == 3) {
-		// We are in a DOM text node.
-		// Replace every occurrence of a real name with an alias.
-		// If the node contains x substrings,
-		// we split this text node into 2x+1 parts.
-		var o = alias_regexp.exec(n.nodeValue);
-		while (o) {
-			// We currently have n.nodeValue == a+b+c,
-			// and b needs to be replaced with f(b).
-			// f(b) is not necessarily just a text string;
-			// it could be an arbitrary sequence of DOM nodes.
-			// Therefore, we split `n` into three parts:
-			// A text node containing `a`, the dom nodes `f(b)`,
-			// and a text node containing `c`.
-			// We have to recurse on `c` (the rest of the text),
-			// so in reality we just insert `a` and `f(b)`
-			// before `n` and replace `n` with `c`.
-
-			// Insert (possibly empty) text node `a` before `n`:
-			var before = document.createTextNode(n.nodeValue.substring(0, o.index));
-			n.parentNode.insertBefore(before, n);
-			var next = before.nextSibling;
-
-			// Insert nodes `f(b)` before n (might be a no-op if f(b) is empty):
-			aliases.replacements[o[0]](n, o[0]);
-
-			var i = before.nextSibling;
-			var oldText = o[0];
-			while (i && i != next) {
-				i.setAttribute('data-tk-prev', oldText);
-				oldText = '';
-				i = i.nextSibling;
+function search_for_names(n) {
+	var to_visit = [n];
+	var l = 1;
+	while (l > 0) {
+		var n = to_visit[--l];
+		if (n.nodeType == 1) {
+			// Recurse through all child nodes of this DOM element.
+			var c = n.lastChild;
+			while (c) {
+				var n = c.previousSibling;
+				to_visit[l++] = c;
+				c = n;
 			}
+		} else if (n.nodeType == 3) {
+			// We are in a DOM text node.
+			// Replace every occurrence of a real name with an alias.
+			// If the node contains x substrings,
+			// we split this text node into 2x+1 parts.
+			var o = alias_regexp.exec(n.nodeValue);
+			while (o) {
+				// We currently have n.nodeValue == a+b+c,
+				// and b needs to be replaced with f(b).
+				// f(b) is not necessarily just a text string;
+				// it could be an arbitrary sequence of DOM nodes.
+				// Therefore, we split `n` into three parts:
+				// A text node containing `a`, the dom nodes `f(b)`,
+				// and a text node containing `c`.
+				// We have to recurse on `c` (the rest of the text),
+				// so in reality we just insert `a` and `f(b)`
+				// before `n` and replace `n` with `c`.
 
-			// Set the text of the `n` node to the remaining (maybe empty) text `c`:
-			n.nodeValue =
-				n.nodeValue.substring(o.index + o[0].length, n.nodeValue.length);
+				// Insert (possibly empty) text node `a` before `n`:
+				var before = document.createTextNode(n.nodeValue.substring(0, o.index));
+				n.parentNode.insertBefore(before, n);
+				var next = before.nextSibling;
 
-			// Find the next occurrence:
-			o = alias_regexp.exec(n.nodeValue);
+				// Insert nodes `f(b)` before n (might be a no-op if f(b) is empty):
+				aliases.replacements[o[0]](n, o[0]);
+
+				var i = before.nextSibling;
+				var oldText = o[0];
+				while (i && i != next) {
+					i.setAttribute('data-tk-prev', oldText);
+					oldText = '';
+					i = i.nextSibling;
+				}
+
+				// Set the text of the `n` node to the remaining (maybe empty) text `c`:
+				n.nodeValue =
+					n.nodeValue.substring(o.index + o[0].length, n.nodeValue.length);
+
+				// Find the next occurrence:
+				o = alias_regexp.exec(n.nodeValue);
+			}
 		}
 	}
 }
@@ -820,7 +843,7 @@ add_aliases(
 
 function activate_fbtk() {
 	compute_alias_regexp();
-	r(document.body);
+	search_for_names(document.body);
 
 	if (window.theTKTitleObserver) {
 		window.theTKTitleObserver.disconnect();
@@ -829,7 +852,7 @@ function activate_fbtk() {
 	window.theTKTitleObserver = new MutationObserver(function (mutations) {
 		for (var i = 0, l = mutations.length; i < l; ++i) {
 			var m = mutations[i];
-			r(m.target);
+			search_for_names(m.target);
 		}
 	});
 
@@ -843,6 +866,7 @@ function activate_fbtk() {
 function deactivate_fbtk() {
 	if (window.theTKTitleObserver) {
 		window.theTKTitleObserver.disconnect();
+		window.theTKTitleObserver = null;
 	}
 	var q = [document.body];
 	var l = 1;
@@ -860,19 +884,54 @@ function deactivate_fbtk() {
 	}
 }
 
+function toggle_fbtk() {
+	if (window.theTKTitleObserver) deactivate_fbtk();
+	else activate_fbtk();
+}
+
+function toggle_fu_prefix() {
+	window['TKconfig']['FUprefix'] = !TK('FUprefix');
+	deactivate_fbtk(); activate_fbtk();
+}
+
 window.TKsetgf = function TKsetgf(year) {
-	window.TKconfig['gf'] = year;
+	window['TKconfig']['gf'] = year;
 	deactivate_fbtk(); activate_fbtk();
 }
 
 window.TKsetup = function TKsetup(config) {
 	for (var k in config) {
-		if (!(k in window.TKconfig)) throw('Unknown key '+k);
+		if (!(k in window['TKconfig'])) throw('Unknown key '+k);
 	}
 	for (var k in config) {
-		window.TKconfig[k] = config[k];
+		window['TKconfig'][k] = config[k];
 	}
 	deactivate_fbtk(); activate_fbtk();
 }
 
 activate_fbtk();
+
+function tk_keypress(e) {
+	if (!e.target || e.target.nodeType != 1) return true;
+	var tgt = e.target;
+	var tag = tgt.tagName.toLowerCase();
+	if (tag == 'textarea' || tag == 'input' || tgt.isContentEditable) return true;
+	var cc = e.charCode;
+	var gf = TK('gf');
+	if (cc == 45) // minus
+		window.TKsetgf(gf-1);
+	else if (cc == 43) // plus
+		window.TKsetgf(gf+1);
+	else if (cc == 42) // asterisk
+		toggle_fbtk();
+	else if (cc == 47) // slash
+		toggle_fu_prefix();
+	else
+		return true;
+
+	e.stopPropagation();
+	e.preventDefault();
+	return false;
+}
+
+window.addEventListener('keypress', tk_keypress, false);
